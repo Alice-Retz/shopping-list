a47b2dacc671f3b2db1e7b429effc09514ad01df
Alice-Retz <alicegretz@gmail.com>
2021-12-17 12:03:17 -0600

    FIXED THE DELETE FUNCTION FINALLY

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,11 +1,11 @@
 import Item from './Item';
 
-export default function ItemList({ items, onChangeItem, OnDelete }) {
+export default function ItemList({ items, onChangeItem, onDelete }) {
   return (
     <ul>
       {items.map((item) => (
         <li key={item.id}>
-          <Item item={item} handleChange={onChangeItem} onDelete={OnDelete} />
+          <Item item={item} handleChange={onChangeItem} onDelete={onDelete} />
         </li>
       ))}
     </ul>
